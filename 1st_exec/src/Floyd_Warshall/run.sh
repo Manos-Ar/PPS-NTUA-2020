bf0087946d19c7f72d2da5fc7b60962debb5906f
manos <arapidismanolis@gmail.com>
2020-11-15 17:41:46 +0200

    change run.sh

--- 1st_exec/src/Floyd_Warshall/run.sh
+++ 1st_exec/src/Floyd_Warshall/run.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
-
-FILE=metrics_${1}.txt
+dir="metrics"
 nthreads=( 1 2 4 8 16 32 64 )
 sizes=( 1024 2048 4096 )
-if [ -f "$FILE" ]; then
-	rm $FILE
+block=( 32 64 128 256 )
+
+if [ ! -d "${dir}" ]; then
+	mkdir metrics
 fi
-for size in "${sizes[@]}";
+
+if [ -d "${dir}/${1}" ]; then
+	rm -rf "${dir}/${1}"
+	mkdir "${dir}/${1}"
+else
+	mkdir "${dir}/${1}"
+fi
+
+
+for b in "${block[@]}";
 do
-	for nthread in "${nthreads[@]}";
+	for size in "${sizes[@]}";
 	do
-		export OMP_NUM_THREADS=${nthread};
-		echo -n "Threads ${nthread} " >> $FILE
-		if [ ${1} == "fw" ]; then
-			./${1} ${size} >> $FILE
-		else
-			./${1} ${size} ${2} >> $FILE
-		fi
+		for nthread in "${nthreads[@]}";
+		do
+			export OMP_NUM_THREADS=${nthread};
+
+			if [ ${1} == "fw" ]; then
+				FILE="${dir}/${1}/metrics_${1}.txt"
+				echo -n "Threads ${nthread} " >> $FILE
+				./${1} ${size} >> $FILE
+			else
+				FILE="${dir}/${1}/metrics_${1}_${b}.txt"
+				echo -n "Threads ${nthread} " >> $FILE
+				./${1} ${size} ${b} >> $FILE
+			fi
+		done
+			echo >> $FILE
 	done
-		echo >> $FILE
+	if [ ${1} == "fw" ]; then
+		break
+	fi
 done
