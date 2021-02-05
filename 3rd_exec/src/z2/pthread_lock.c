1a7d289542d20a36bf0fc79e7dc02756e2f9d825
manos <arapidismanolis@gmail.com>
2021-02-06 00:02:45 +0200

    fixed pthreads lock

--- 3rd_exec/src/z2/pthread_lock.c
+++ 3rd_exec/src/z2/pthread_lock.c
@@ -5,22 +5,35 @@
 
 
 struct lock_struct {
-	pthread_spinlock_t *spin_lock;
+	pthread_spinlock_t spin_lock;
 };
 
 lock_t *lock_init(int nthreads)
 {
+	int ret;
 	lock_t *lock;
-	// int pshared=0;
 	XMALLOC(lock, 1);
-
-	while(pthread_spin_init(lock->spin_lock,0)){}; //success  == 0
-	return lock;
+	ret=pthread_spin_init(&lock->spin_lock,PTHREAD_PROCESS_PRIVATE);
+	if(!ret) 
+		return lock;
+	else if(ret==EAGAIN){
+		fprintf(stderr, "Out of memory: %s:%d\n", __FILE__, __LINE__); 
+		exit(1); 
+	}
+	else if(ret==EBUSY){
+		fprintf(stderr, "Is already iniatialized: %s:%d\n", __FILE__, __LINE__);
+		exit(1); 
+	}
+	else if(ret==EINVAL){
+		fprintf(stderr, "Invalid lock: %s:%d\n", __FILE__, __LINE__);
+		exit(1);
+	}
+	return NULL;
 }
 
 void lock_free(lock_t *lock)
 {
-	pthread_spin_destroy(lock->spin_lock);
+	pthread_spin_destroy(&lock->spin_lock);
 	XFREE(lock);
 }
 
@@ -28,11 +41,11 @@ void lock_acquire(lock_t *lock)
 {
 	int ret;
 	while(1){
-		ret = pthread_spin_lock(lock->spin_lock);
+		ret = pthread_spin_lock(&lock->spin_lock);
 		if(ret==0) break;
 		else if(ret==EDEADLK) break;
 		else if(ret==EINVAL){
-			fprintf(stderr, "Undefined spinlock: %s:%d\n", __FILE__, __LINE__);
+			fprintf(stderr, "Uninitialized spinlock: %s:%d\n", __FILE__, __LINE__);
 			exit(1);
 		}
 	};
@@ -41,15 +54,15 @@ void lock_acquire(lock_t *lock)
 void lock_release(lock_t *lock)
 {
 	int ret;
-	ret=pthread_spin_lock(lock->spin_lock);
-	if(ret==0) return;
+	ret=pthread_spin_unlock(&lock->spin_lock);
+	if(ret==0)
+		return;
 	else if(ret==EPERM){
 		fprintf(stderr, "Thread does not hold the spinlock: %s:%d\n", __FILE__, __LINE__);
 			exit(1);
 	}
 	else if(ret==EINVAL){
-			fprintf(stderr, "Undefined spinlock: %s:%d\n", __FILE__, __LINE__);
+			fprintf(stderr, "Uninitialized spinlock: %s:%d\n", __FILE__, __LINE__);
 			exit(1);
-		}
-	
+	}	
 }
